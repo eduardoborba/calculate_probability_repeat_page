e4d35039f03c1b9cd81d48b75900bd6cc1445f89
Eduardo Borba <eduardo@jobscore.com>
2017-07-10 10:19:51 -0300

    Change to results to file

--- lib/calculate_probability_repeat_page.rb
+++ lib/calculate_probability_repeat_page.rb
@@ -3,6 +3,14 @@ require "pry-byebug"
 
 class CalculateProbabilityRepeatPage
   def self.perform(file_path)
+    file_content = CSV.generate_line([
+      "id_usuario",
+      "probability_repeat_page",
+      "probability_repeat_page_except_initial",
+      "number_pages_repeated",
+      "number_pages_repeated_except_inital",
+      "number_pages_accessed"
+    ])
     current_user = nil
     pages_accessed = []
     number_pages_repeated = 0.0
@@ -14,12 +22,14 @@ class CalculateProbabilityRepeatPage
     CSV.foreach(file_path, headers: true) do |row|
       if row['id_usuario'] != current_user
         unless current_user.nil?
-          probability_repeat_page[current_user] = number_pages_repeated / pages_accessed.count
-          probability_repeat_page_except_initial[current_user] = number_pages_repeated_except_inital / pages_accessed.count
-          puts "Probability for user #{current_user} = #{probability_repeat_page[current_user]}"
-          puts "Probability (except initial) for user #{current_user} = #{probability_repeat_page_except_initial[current_user]}"
-          puts "Number pages repeated = #{number_pages_repeated}"
-          puts "Number pages accessed = #{pages_accessed.count}"
+          line = []
+          line << current_user
+          line << number_pages_repeated / pages_accessed.count
+          line << number_pages_repeated_except_inital / pages_accessed.count
+          line << number_pages_repeated
+          line << number_pages_repeated_except_inital
+          line << pages_accessed.count
+          file_content << CSV.generate_line(line)
         end
         current_user = row['id_usuario']
         pages_accessed = []
@@ -39,12 +49,18 @@ class CalculateProbabilityRepeatPage
 
       pages_accessed << row['idaction_url']
     end
-    probability_repeat_page[current_user] = number_pages_repeated / pages_accessed.count
-    probability_repeat_page_except_initial[current_user] = number_pages_repeated_except_inital / pages_accessed.count
-    puts "Probability for user #{current_user} = #{probability_repeat_page[current_user]}"
-    puts "Probability (except initial) for user #{current_user} = #{probability_repeat_page_except_initial[current_user]}"
-    puts "Number pages repeated = #{number_pages_repeated}"
-    puts "Number pages accessed = #{pages_accessed.count}"
+    line = []
+    line << current_user
+    line << number_pages_repeated / pages_accessed.count
+    line << number_pages_repeated_except_inital / pages_accessed.count
+    line << number_pages_repeated
+    line << number_pages_repeated_except_inital
+    line << pages_accessed.count
+    file_content << CSV.generate_line(line)
+
+    File.open("#{Dir.pwd}/data/probability_repeat_page.csv", 'w') do |file_handler|
+      file_handler.write(file_content)
+    end
   end
 end
 
